51b22b195c5fcbc283e2434b1839d88b2096e9ce
Alija Sabic <sabic.alija@gmail.com>
2021-10-04 12:33:54 +0200

    Implement deleting note functionality

--- index.js
+++ index.js
@@ -20,9 +20,16 @@ function addToDo() {
     
     if (input.value !== "") {
         const item = document.createElement("li");
+        item.addEventListener("click", removeToDo);
         item.textContent = input.value;
         input.value = "";
         list.appendChild(item);
         input.focus();
     } 
+}
+
+function removeToDo(event) {
+    const list = document.getElementById("todo-list");
+    list.removeChild(event.target);
+    // event.target.parentNode.removeChild(event.target);
 }
