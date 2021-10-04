6c4145e43d5446398f9c1809cd2745cccccf5851
Alija Sabic <sabic.alija@gmail.com>
2021-10-04 12:00:18 +0200

    Register event handler for input, refactor adding note

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const button = document.getElementById("add-todo");
 button.addEventListener("click", handleClick);
 
+const input = document.getElementById("todo-note");
+input.addEventListener("keydown", handleKeyDown);
+
 function handleClick(event) {
+    addToDo();
+}
+
+function handleKeyDown(event) {
+    if (event.code === "Enter") {
+        addToDo();
+    }
+}
+
+function addToDo() {
     const input = document.getElementById("todo-note");
     const list = document.getElementById("todo-list");
     
-    const item = document.createElement("li");
-    item.textContent = input.value;
-
-    list.appendChild(item);
+    if (input.value !== "") {
+        const item = document.createElement("li");
+        item.textContent = input.value;
+        input.value = "";
+        list.appendChild(item);
+        input.focus();
+    } 
 }
